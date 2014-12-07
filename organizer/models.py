b35fe2b1b24f9aac27bc35a43f884d05d458211d
Connor McBride <cmm863@mst.edu>
2014-12-07 13:08:37 -0600

    Added location and tournament entities.

--- organizer/models.py
+++ organizer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=2)
+    address = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = (("city", "state", "address"),)
+
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=25)
+    location = models.ForeignKey('Location')
