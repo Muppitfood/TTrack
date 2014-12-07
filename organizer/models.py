34491a9d5667dd881ef89cbccf8a43421c2c34ce
Connor McBride <cmm863@mst.edu>
2014-12-07 13:29:31 -0600

    Added all models

--- organizer/models.py
+++ organizer/models.py
@@ -15,3 +15,25 @@ class Location(models.Model):
 class Tournament(models.Model):
     name = models.CharField(max_length=25)
     location = models.ForeignKey('Location')
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+
+class Manager(models.Model):
+    name = models.CharField(max_length=20)
+    team = models.ForeignKey('Team')
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    role = models.CharField(max_length=10)
+    bio = models.TextField()
+    team = models.ForeignKey('Team')
+
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    contract_end = models.DateField(db_index=True)
+    contribution_size = models.DecimalField(max_digits=10, decimal_places=2)
